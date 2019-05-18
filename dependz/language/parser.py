de6770b49f041609d49c9c387471a76fd43282fe
Romain Beguet <beguet@adacore.com>
2019-05-18 20:39:18 +0200

    Implement evaluator.

--- dependz/language/parser.py
+++ dependz/language/parser.py
@@ -6,11 +6,21 @@ from langkit.dsl import (
 )
 from langkit.envs import EnvSpec, add_env, add_to_env_kv, handle_children
 from langkit.expressions import (
-    Self, langkit_property, Property, AbstractProperty, Not, No, If,
-    ArrayLiteral, String
+    Self, Entity, langkit_property, Property, AbstractProperty, Not, No, If,
+    ArrayLiteral, String, Var, AbstractKind, Let
 )
 from langkit.parsers import Grammar, List, Or, Pick
 from language.lexer import dependz_lexer as L
+
+
+class Substitution(Struct):
+    sym = UserField(type=T.Symbol)
+    actual = UserField(type=T.Term.entity)
+
+
+class Result(Struct):
+    value = UserField(type=T.Term.entity)
+    context = UserField(type=T.Substitution.array)
 
 
 @abstract
@@ -43,7 +53,15 @@ class DefTerm(DependzNode):
 
 @abstract
 class Term(DefTerm):
-    pass
+    @langkit_property(public=False, return_type=T.Result,
+                      kind=AbstractKind.abstract)
+    def eval(ctx=T.Substitution.array):
+        pass
+
+    @langkit_property(return_type=T.Term, public=True,
+                      kind=AbstractKind.abstract)
+    def substitute(old=T.Symbol, by=T.Symbol):
+        pass
 
 
 @abstract
@@ -52,9 +70,38 @@ class Identifier(Term):
 
     to_string = Property(Self.sym.image)
 
+    @langkit_property(public=True, return_type=T.Bool)
+    def is_introducing():
+        return Self.parent.is_a(Introduction)
+
     @langkit_property(public=True, return_type=T.Introduction.entity)
     def intro():
         return Self.node_env.get_first(Self.sym).cast(Introduction)
+
+    @langkit_property()
+    def eval(ctx=T.Substitution.array):
+        return ctx.find(lambda s: s.sym == Self.sym).then(
+            lambda s: Result.new(
+                value=s.actual,
+                context=ctx
+            ),
+            default_val=Self.intro.definition.then(
+                lambda d: d.eval.then(lambda r: Result.new(
+                    value=r.value,
+                    context=r.context.concat(ctx)
+                )),
+                default_val=Result.new(
+                    value=Entity,
+                    context=ctx
+                )
+            )
+        )
+
+    @langkit_property()
+    def substitute(old=T.Symbol, by=T.Symbol):
+        return Self.parent.make_ident(
+            If(Self.sym == old, by, Self.sym)
+        )
 
 
 @synthetic
@@ -77,6 +124,29 @@ class Apply(Term):
         Self.lhs.to_string.concat(String(' ')).concat(Self.rhs.to_string)
     ).concat(String(")")))
 
+    @langkit_property()
+    def eval(ctx=T.Substitution.array):
+        evaled_lhs = Var(Entity.lhs.eval(ctx))
+        evaled_rhs = Var(Entity.rhs.eval(evaled_lhs.context))
+
+        return evaled_lhs.value.cast(T.Abstraction).then(
+            lambda abs: abs.apply(evaled_rhs.context, evaled_rhs.value),
+            default_val=Result.new(
+                value=Self.parent.make_apply(
+                    evaled_lhs.value.node,
+                    evaled_rhs.value.node
+                ).as_entity,
+                context=evaled_rhs.context
+            )
+        )
+
+    @langkit_property()
+    def substitute(old=T.Symbol, by=T.Symbol):
+        return Self.parent.make_apply(
+            Self.lhs.substitute(old, by),
+            Self.rhs.substitute(old, by)
+        )
+
 
 @synthetic
 class Abstraction(Term):
@@ -86,6 +156,36 @@ class Abstraction(Term):
     to_string = Property(String("(\\").concat(
         Self.ident.to_string.concat(String('. ')).concat(Self.term.to_string)
     ).concat(String(")")))
+
+    @langkit_property()
+    def eval(ctx=T.Substitution.array):
+        return Result.new(
+            value=Entity,
+            context=ctx
+        )
+
+    @langkit_property()
+    def apply(ctx=T.Substitution.array, val=T.Term.entity):
+        fresh_id = Var(Self.fresh_symbol(Self.ident.sym))
+
+        subst = Var(Substitution.new(
+            sym=fresh_id,
+            actual=val
+        ).singleton)
+
+        return (
+            Entity.term
+            .substitute(Self.ident.sym, fresh_id)
+            .as_entity
+            .eval(subst.concat(ctx))
+        )
+
+    @langkit_property()
+    def substitute(old=T.Symbol, by=T.Symbol):
+        return Self.parent.make_abstraction(
+            Self.ident.substitute(old, by).cast(Identifier),
+            Self.term.substitute(old, by)
+        )
 
 
 class Arrow(DefTerm):
@@ -120,6 +220,14 @@ class Definition(DependzNode):
     """
     ident = Field(type=SourceId)
     term = Field(type=Term)
+
+    @langkit_property(public=True, return_type=T.Term.entity)
+    def value():
+        return Entity.eval.value
+
+    @langkit_property(return_type=Result, memoized=True)
+    def eval():
+        return Entity.term.eval(No(Substitution.array))
 
     env_spec = EnvSpec(
         handle_children(),

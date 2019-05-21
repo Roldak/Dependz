666d03548d4cbb3b0fe3a2bf49166a0ef944a4f7
Romain Beguet <beguet@adacore.com>
2019-05-21 10:19:08 +0200

    Add testsuite for domain checker and simple test.

---
+++ testsuite/domain_checker/run.py
@@ -0,0 +1,47 @@
+import libdependzlang as ldl
+
+import os
+import sys
+
+
+def defs_to_domain_check(unit):
+    return unit.root.findall(
+        lambda n: n.is_a(ldl.Definition) and "test" in n.f_ident.text
+    )
+
+
+def run(src_file):
+    ctx = ldl.AnalysisContext()
+    u = ctx.get_from_file(src_file)
+
+    def set_debug(v):
+        u.root.p_set_logic_equation_debug_mode(1 if v else 0)
+
+    assert not u.diagnostics
+
+    for d in defs_to_domain_check(u):
+        def_name = d.f_ident.text
+
+        try:
+            r = d.p_check_domains
+        except ldl.PropertyError:
+            print("A crash occurred while domain checking {}.".format(
+                def_name
+            ))
+            continue
+
+        if r:
+            print("{}: Success".format(def_name))
+            for t in [d.f_term] + d.f_term.findall(ldl.Term):
+                dom = t.p_domain
+                print("  {}: {}".format(t, dom.p_to_string if dom else "None"))
+        else:
+            set_debug(True)
+            print("{} : Failed".format(def_name))
+            d.p_check_domains
+            set_debug(False)
+
+
+
+if __name__ == "__main__":
+    run(os.path.join(sys.argv[1], "test.dep"))

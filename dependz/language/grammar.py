891ab47eedb5ee9d8c6ac6483e6ab19e07d6b902
Romain Beguet <beguet@adacore.com>
2019-10-05 17:55:41 +0200

    Update grammar to allow Arrow as terms.

--- dependz/language/grammar.py
+++ dependz/language/grammar.py
@@ -15,19 +15,17 @@ dependz_grammar.add_rules(
     main_rule=List(D.toplevel, empty_valid=True, list_cls=Program),
     toplevel=Or(D.intro, D.definition),
     intro=Introduction(D.ident, ':', D.defterm, L.Newlines),
-    definition=Definition(D.ident, '=', D.term, L.Newlines),
+    definition=Definition(D.ident, '=', D.defterm, L.Newlines),
 
     ident=SourceId(L.Ident),
 
+    defterm=Or(
+        Or(Arrow(Null(Term), D.term, '->', D.defterm),
+           Arrow(D.term, ':', D.term, '->', D.defterm)),
+        D.term
+    ),
     term=Or(Apply(D.term, D.term1), D.term1),
     term1=Or(D.ident, D.term2),
     term2=Or(Abstraction('\\', D.ident, '.', D.term), D.term3),
-    term3=Pick('(', D.term, ')'),
-
-    defterm=Or(
-        Or(Arrow(Null(Term), D.defterm1, '->', D.defterm),
-           Arrow(D.term, ':', D.defterm1, '->', D.defterm)),
-        D.defterm1
-    ),
-    defterm1=Or(Pick("(", D.defterm, ")"), D.term),
+    term3=Pick('(', D.defterm, ')')
 )
